729953fdab0e93dfbe1574dff4e0a1311e7d07ac
Giampietri Gonzalo <Giampietri.Gonzalo@protonmail.com>
2019-06-15 17:59:55 -0300

    Heap y DisjointSet implementados

---
+++ AyC_Actividad15/src/DisjoinSet.java
@@ -0,0 +1,59 @@
+public class DisjoinSet {
+
+	private int[] parent;
+	private int[] rank;
+	private int size;
+	
+	public DisjoinSet(int maximumElements) {
+		parent = new int [maximumElements];
+		rank = new int[maximumElements];
+		size = maximumElements;
+		
+		for (int i=0; i<maximumElements; i++) {
+			makeSet(i);
+		}
+			
+	}
+	
+	public void makeSet(int element){
+		parent[element] = element;
+		rank[element] = 0;
+	}
+
+	public int findSet(int element) {
+		if (parent[element] != element)
+			parent[element] = findSet(parent[element]);
+		return parent[element];
+	}
+	
+	public void union(int elementX, int elementY) {
+		link(findSet(elementX), findSet(elementY));
+	}
+	
+	private void link(int elementX, int elementY) {
+		if (rank[elementX] > rank[elementY])
+			parent[elementY] = elementX;
+		else
+			parent[elementX] = elementY;
+		
+		if (rank[elementX] == rank[elementY])
+			rank[elementY] += 1;
+	}
+
+	/**
+	 * Determian si el grafo en el DisjointSet es conexo
+	 * */
+	/*public boolean isRelated() {
+		int root = findSet(parent[0]);
+		boolean isRelated = true;
+
+		for (int i = 1; i < parent.length; i++) {
+			if (findSet(parent[i]) != root)
+				isRelated = false;
+		}
+		
+		return isRelated;
+	}*/
+
+
+}
--- AyC_Actividad15/src/Heap.java
+++ AyC_Actividad15/src/Heap.java
@@ -1,4 +1,4 @@
-public class Heap {
-
-
+public interface Heap {
+    void insert(int element);
+    int get();
 }
