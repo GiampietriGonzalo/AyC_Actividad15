381394355dbb86d0d8730c54686fdb643f6caf24
Tomas Perotti <jguidi@rccl.com>
2019-06-17 15:49:27 -0300

    Disjoint-Set improved and classic implemented.

--- AyC_Actividad15/src/DisjointSet.java
+++ AyC_Actividad15/src/DisjointSet.java
@@ -1,72 +1,129 @@
-//TODO FALTA DISJOINTSET SIN HEURISTICA
+interface IDisjointSet {
+	int find(int i);
+	void union(int i, int j);
+}
+
+
+class ClassicDisjointSet implements IDisjointSet {
+
+	int[] parent;
+	int n;
+
+	public ClassicDisjointSet (int n) {
+		parent = new int[n];
+		this.n = n;
+		makeSet();
+	}
+
+	private void makeSet()
+	{
+		for (int i = 0; i < n; i++) {
+			this.parent[i] = i;
+		}
+	}
+
+	// Finds the representative of the set that i is an element of
+	public int find(int x)
+	{
+		if (parent[x] == x)
+		{
+			return x;
+		}
+		else
+		{
+			return find(parent[x]);
+		}
+	}
+
+	// Unites the set that includes i and the set that includes j
+	public void union(int x, int y)
+	{
+		int i_representative = this.find(x);
+		int j_representative = this.find(y);
+
+		parent[i_representative] = j_representative;
+	}
+
+}
 
 /**
- * DisjointSet con compression path y union-by-rank.
- * */
-class DisjointSet {
+ * Esta clase implementa Disjoint-Set con Union by Rank y Path compression.
+ */
+class ImprovedDisjointSet implements IDisjointSet {
 
-	private int[] parent;
-	private int[] rank;
-	private int size;
-	
-	public DisjointSet(int maximumElements) {
-		parent = new int [maximumElements];
-		rank = new int[maximumElements];
-		size = maximumElements;
-		
-		for (int i=0; i<maximumElements; i++) {
-			makeSet(i);
+	int[] parent, rank;
+	int n;
+
+	public ImprovedDisjointSet (int n) {
+		parent = new int[n];
+		rank = new int[n];
+		this.n = n;
+		makeSet();
+	}
+
+	private void makeSet()
+	{
+		for (int i = 0; i < n; i++) {
+			this.parent[i] = i;
 		}
 	}
-	
-	public void makeSet(int element){
-		parent[element] = element;
-		rank[element] = 1;
+
+	public int find(int x)
+	{
+		if (parent[x] != x) {
+			parent[x] = find(parent[x]);
+		}
+
+		return parent[x];
 	}
 
-	public int findSet(int element) {
-		if (parent[element] != element)
-			parent[element] = findSet(parent[element]);
-		return parent[element];
-	}
-	
-	public void union(int elementX, int elementY) {
-		int x = findSet(elementX);
-		int y = findSet(elementY);
+	public void union(int x, int y)
+	{
+		int xRoot = find(x), yRoot = find(y);
 
-		if (rank[x] > rank[x])
-			parent[y] = x;
+		if (xRoot == yRoot)
+			return;
+
+		if (rank[xRoot] < rank[yRoot])
+			parent[xRoot] = yRoot;
 		else
-			parent[x] = y;
-
-		if (rank[x] == rank[y])
-			rank[y] += 1;
+			if (rank[yRoot] < rank[xRoot])
+				parent[yRoot] = xRoot;
+			else {
+				parent[yRoot] = xRoot;
+				rank[xRoot] = rank[xRoot] + 1;
+			}
 	}
 
-	/**
-	 * Determina si el grafo en el DisjointSet es conexo
-	 * CHECKEAR
-	 * */
-	public boolean esConexo() {
-		int root = findSet(parent[0]);
-		boolean esConexo = true;
+}
 
-		int j = 0;
-		while (esConexo && j<parent.length) {
-			if(findSet(j) != root)
-				esConexo = false;
-			j++;
-		}
+class MainDisjointSet {
+	public static void main(String[] args)
+	{
+		// Let there be 5 persons with ids as
+		// 0, 1, 2, 3 and 4
+		int n = 5;
+		IDisjointSet ds = new ImprovedDisjointSet(n);
 
-		
-		return esConexo;
-	}
+		// 0 is a friend of 2
+		ds.union(0, 2);
 
-	public String toString() {
-		String toReturn = "";
-		for (int i=0; i<parent.length; i++)
-			toReturn += "nodo:" + i + " parent:" + parent[i] + "\n";
+		// 4 is a friend of 2
+		ds.union(4, 2);
 
-		return toReturn;
+		// 3 is a friend of 1
+		ds.union(3, 1);
+
+		// Check if 4 is a friend of 0
+		if (ds.find(4) == ds.find(0))
+			System.out.println("Is 4 friend of 0 ? Yes");
+		else
+			System.out.println("Is 4 friend of 0 ? No");
+
+		// Check if 1 is a friend of 0
+		if (ds.find(1) == ds.find(0))
+			System.out.println("Is 1 friend of 0 ? Yes");
+		else
+			System.out.println("Is 1 friend of 0 ? No");
 	}
 }
